563794c158eaa827edab2a93a283ca67667279b3
devopscomputer <pauloti.engenharia@gmail.com>
2026-06-08 20:23:50 -0300

    feat: update src/api-client/index.ts

--- src/api-client/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./generated/api";
-export * from "./generated/api.schemas";
-export * from "./sniper-hooks";
-export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
-export type { AuthTokenGetter } from "./custom-fetch";
